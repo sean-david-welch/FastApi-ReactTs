006569645642614a388106bb3f6b75d3b64977ed
Sean Welch <seanwelch51199@icloud.com>
2023-05-08 16:12:59 +0100

    fixed environement variables

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,7 +1,19 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const FRONTEND_BASE_URL =
-    import.meta.env.VITE_FRONTEND_BASE_URL ||
-    import.meta.env.VITE_FRONTEND_BASE_FALLBACK_URL;
+
+let FRONTEND_BASE_URL: string;
+
+if (import.meta.env.MODE === 'production') {
+    FRONTEND_BASE_URL = import.meta.env.VITE_PRODUCTION_BASE_URL;
+} else {
+    FRONTEND_BASE_URL =
+        import.meta.env.VITE_FRONTEND_BASE_URL ||
+        import.meta.env.VITE_FRONTEND_BASE_FALLBACK_URL;
+}
+
+export { FRONTEND_BASE_URL };
+
+console.log('FRONTEND_BASE_URL', FRONTEND_BASE_URL);
+console.log('MODE', import.meta.env.MODE);
 
 export const STRIPE_PUBLIC_KEY = import.meta.env.VITE_STRIPE_PUBLIC_KEY;
 
